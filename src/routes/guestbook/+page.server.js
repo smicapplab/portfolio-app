d2b98ac60aa9fd4237a47a440313a7822a7a8951
Steve Torrefranca <s.torrefranca@gmail.com>
2024-09-06 22:18:27 +0800

    remove sendgrid

--- src/routes/guestbook/+page.server.js
+++ src/routes/guestbook/+page.server.js
@@ -2,8 +2,7 @@ import { fail } from '@sveltejs/kit';
 import { superValidate } from 'sveltekit-superforms/server';
 import { zod } from 'sveltekit-superforms/adapters';
 import { guestBookSchema } from '$lib/schemas/contact';
-import sgMail from '@sendgrid/mail';
-import { SENDGRID_API_KEY } from '$env/static/private';
+// import { SENDGRID_API_KEY } from '$env/static/private';
 
 export const actions = {
 	leaveMessage: async ({ request }) => {
@@ -17,28 +16,32 @@ export const actions = {
 			return fail(400, { form });
 		}
 
-		const { fullName, email, message } = form.data;
-
-		sgMail.setApiKey(SENDGRID_API_KEY);
-		const msg = {
-			to: 's.torrefranca@gmail.com',
-			from: 's.torrefranca@gmail.com', // Must be verified in SendGrid
-			subject: 'Portfolio Guestbook',
-			text: `
-			From: ${fullName}
-			Email: ${email}
-			Message:
-			${message}`
+		return {
+			form
 		};
 
-		try {
-			await sgMail.send(msg);
-			return {
-				form
-			};
-		} catch (error) {
-			console.error(error);
-			return fail(400, { form });
-		}
+		// const { fullName, email, message } = form.data;
+
+		// sgMail.setApiKey(SENDGRID_API_KEY);
+		// const msg = {
+		// 	to: 's.torrefranca@gmail.com',
+		// 	from: 's.torrefranca@gmail.com', // Must be verified in SendGrid
+		// 	subject: 'Portfolio Guestbook',
+		// 	text: `
+		// 	From: ${fullName}
+		// 	Email: ${email}
+		// 	Message:
+		// 	${message}`
+		// };
+
+		// try {
+		// 	await sgMail.send(msg);
+		// 	return {
+		// 		form
+		// 	};
+		// } catch (error) {
+		// 	console.error(error);
+		// 	return fail(400, { form });
+		// }
 	}
 };
